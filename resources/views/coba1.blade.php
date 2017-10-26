817695d96c54ffa8251dad06a24f809844050c9b
regytadhea <regytadhea@gmail.com>
2017-10-26 16:50:07 +0700

    26 Oktober 2017

---
+++ resources/views/coba1.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master') <!--menggunakan file master di folder layout, di contoh ini semua file di master disesuaikan antara master dg coba 1--->
+
+@section('content_header')
+<h1>
+	Coba 1
+	<small>Control panel</small>
+</h1>
+<ol class="breadcrumb">
+	<li><a href="/"><i class="fa fa-dashboard"></i> Home</a></li>
+	<li class="active">Coba 1</li>
+</ol>
+@endsection
+
+@section('content')
+Halo ini coba 1
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,6 +90,7 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
 					<a href="percakapan">Percakapan</a>
 					<a href="contact">Contact</a>
+					<a href="template">Templating</a>
                 </div>
             </div>
         </div>
