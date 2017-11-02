93079609a4c4a9d553bbd486ee6f09b5bb36e04e
regytadhea <regytadhea@gmail.com>
2017-11-02 16:07:15 +0700

    push 4

--- routes/web.php
+++ routes/web.php
@@ -40,10 +40,17 @@ Route::get('coba2', function () {
 });
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home'); //kalo berhasil login masuk ke home
+/*Route::get('/home', 'HomeController@index')->name('home'); //kalo berhasil login masuk ke home
 
 Route::get('/admin', 'AdminController@index')->name('admin.dashboard'); //routing ke admin
 
-Route::get('/admin/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+Route::get('/admin/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login'); //route untuk admin login
 
-Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit'); //routes untuk admin login submit
+*/
+
+Route::prefix('admin')->group(function() {
+	Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+	Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+	Route::get('/', 'AdminController@index')->name('admin.dashboard');
+});
